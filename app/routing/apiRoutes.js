bc47bb047e6426d09145e84686807fc66f094631
Robert Bolton <therobshock@yahoo.com>
2019-04-30 15:44:17 -0700

    data on submit to api route. friend object okay. modal initial setup.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,10 +3,12 @@ var friends = require("../data/friends");
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
-        res.json(friends);
+        return res.json(friends);
     });
 
     app.post("/api/friends", function(req, res) {
+        console.log(req.body);
         friends.push(req.body);
+        res.json(req.body);
     })
 }
