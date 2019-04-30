6dd001d6e38ed1a02bbb02f1e50bee94fccaf959
Robert Bolton <therobshock@yahoo.com>
2019-04-30 14:56:10 -0700

    all routes working

--- app/server.js
+++ app/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-// require("./routing/apiRoutes")(app);
+require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
 // =============================================================================
