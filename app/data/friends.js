04932da35d00c9f0a3c6a08023195070f23b72d0
Robert Bolton <therobshock@yahoo.com>
2019-04-30 18:03:10 -0700

    beggining of comparison logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,19 @@
-var friendsData = [];
+var friendsData = [
+    {
+        name: "Secret Samantha",
+        photo: "https://i.ytimg.com/vi/OaxqPszajw0/maxresdefault.jpg",
+        scores: [1, 3, 2, 5, 4, 2, 4, 5, 2, 1]
+    },
+    {
+        name: "Mulan",
+        photo: "https://i.pinimg.com/originals/07/4c/09/074c09020e958fcec0ddcb83afce83a4.jpg",
+        scores: [3, 5, 1, 1, 3, 2, 5, 4, 4, 3]
+    },
+    {
+        name: "Adiffe Renkine O'Rick",
+        photo: "https://www.tvovermind.com/wp-content/uploads/2018/08/rick-sanchez-750x421-750x421.jpg",
+        scores: [5, 1, 1, 5, 5, 5, 1, 5, 1, 1]
+    }
+];
 
 module.exports = friendsData;
